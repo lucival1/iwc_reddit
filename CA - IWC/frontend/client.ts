2c6b479a13b2d556e278649054cee2ec888d6dad
lucivalnascimento <lucival1@gmail.com>
2019-01-16 20:33:28 +0000

    getAuth client

--- CA - IWC/frontend/client.ts
+++ CA - IWC/frontend/client.ts
@@ -1,13 +1,20 @@
-const data = {
-    userId: 1,
-    content: "some content"
-}
+// fetch JWT TOKEN
+let getAuth = (async (email_: string, password_: string) => {
+    const userData = {
+        email: email_,
+        password: password_
+    };
 
-fetch("localhost:8080/api/v1/links",
-    {
-        method: "POST",
-        headers: {
-            "x-auth-token": "some token"
-        },
-        body: "some body"
-    });
+    const response = await fetch(
+        "http://localhost:8080/api/v1/auth/login",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData)
+        }
+    );
+    const json = await response.json();
+    console.log(json);
+});
