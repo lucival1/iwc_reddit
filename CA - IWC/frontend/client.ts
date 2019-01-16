997f66488c488c1cac8b7382267c2e16700dcc70
lucivalnascimento <lucival1@gmail.com>
2019-01-16 21:16:05 +0000

    link routes web client

--- CA - IWC/frontend/client.ts
+++ CA - IWC/frontend/client.ts
@@ -52,3 +52,100 @@ let newUser = (async (email_: string, password_: string) => {
     const json = await response.json();
     console.log(json);
 });
+
+
+// fetch all links data
+let getLinks = (async () => {
+    const response = await fetch(
+        "http://localhost:8080/api/v1/links/",
+        {
+            method: "GET"
+        }
+    );
+    const json = await response.json();
+    console.log(json);
+});
+
+
+// fetch link data
+let getLink = (async (linkId: number) => {
+    const response = await fetch(
+        "http://localhost:8080/api/v1/links/" + linkId,
+        {
+            method: "GET"
+        }
+    );
+    const json = await response.json();
+    console.log(json);
+});
+
+
+// post a new link
+let postLink = (async (title_: string, url_: string) => {
+    const linkData = {
+        title: title_,
+        url: url_
+    };
+
+    const response = await fetch(
+        "http://localhost:8080/api/v1/links",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ0MDA2NDc1fQ.VeTElpEPlPzefezB4PXtZGSPzutEGLfefaEAwJjIob4"
+            },
+            body: JSON.stringify(linkData)
+        }
+    );
+    const json = await response.json();
+    console.log(json);
+});
+
+
+// delete a link
+let deleteLink = (async (linkId: number) => {
+    const response = await fetch(
+        "http://localhost:8080/api/v1/links/" + linkId,
+        {
+            method: "DELETE",
+            headers: {
+                "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ0MDA2NDc1fQ.VeTElpEPlPzefezB4PXtZGSPzutEGLfefaEAwJjIob4"
+            }
+        }
+    );
+    const json = await response.json();
+    console.log(json);
+});
+
+
+// upvote a link
+let upvoteLink = (async (linkId: number) => {
+    const response = await fetch(
+        "http://localhost:8080/api/v1/links/" + linkId + "/upvote",
+        {
+            method: "POST",
+            headers: {
+                "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ0MDA2NDc1fQ.VeTElpEPlPzefezB4PXtZGSPzutEGLfefaEAwJjIob4"
+            }
+        }
+    );
+    const json = await response.json();
+    console.log(json);
+});
+
+
+// downvote a link
+let downvoteLink = (async (linkId: number) => {
+    const response = await fetch(
+        "http://localhost:8080/api/v1/links/" + linkId + "/downvote",
+        {
+            method: "POST",
+            headers: {
+                "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ0MDA2NDc1fQ.VeTElpEPlPzefezB4PXtZGSPzutEGLfefaEAwJjIob4"
+            }
+        }
+    );
+    const json = await response.json();
+    console.log(json);
+});
